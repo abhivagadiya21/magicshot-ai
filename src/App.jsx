c85d5ba806fc2ce1ffb925925cbe8759d35df357
Abhi Vagadiya <your-email@example.com>
2025-07-25 17:41:17 +0530

    feat: Add AgeJourney and ChangeHaircut pages with corresponding styles and images

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import './App.css'
 import React from 'react'
 import Headers from './components/heding/heding.jsx'
 import BabyPage from './pages/BabyGenrator_page/babyPage.jsx';
-import { Routes, Route } from 'react-router-dom';
+import ChagehaircutPage from './pages/ChangeHaircut_page/chnagehaircut.jsx';
+import AgeJourney from './pages/AgeJourney_page/Agejourney.jsx';
+import AgePredictor from './pages/AgePredictor_page/AgePredictor.jsx';
 
 function App() {
   return (
     <>
     <Headers />
-     <Routes>
-      <Route path="/" element={<BabyPage />} />
-    </Routes>
+      {/* <BabyPage /> */}
+      {/* <ChagehaircutPage /> */}
+      {/* <AgeJourney /> */}
+      {/* <AgePredictor /> */}
     </>
   )
 }
--- src/components/upload_img_re_compo/Upload_img.jsx
+++ src/components/upload_img_re_compo/Upload_img.jsx
@@ -1,11 +1,11 @@
 import './Upload_img.css';
-import babyG from './image_that/babyG.png'
-function Upload_img() {
+// import babyG from './image_that/babyG.png'
+function Upload_img({uploadDetails}) {
     return (
         <>
             <div className="main-upload-img-babyG">
                 <div className="inner-1-img-babyG">
-                    <img src={babyG} alt="Baby Generator" className='upload-img-props ' />
+                    <img src={uploadDetails.image} alt="Baby Generator" className='upload-img-props ' />
                 </div>
                 <div class="upload-container">
                     <span class="plus-icon">+</span>
