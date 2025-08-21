da9ffbb442d970c14c6641e0fa7b140d01c45e9f
Abhi Vagadiya <your-email@example.com>
2025-08-21 18:54:43 +0530

    feat: implement age prediction functionality and update API integration in AgePredictor component

--- src/pages/AgePredictor_page/AgePredictor.jsx
+++ src/pages/AgePredictor_page/AgePredictor.jsx
@@ -1,132 +1,186 @@
-import usePopup from '../../hooks/usePopup';
-import Upload_img from '../../components/upload_img_re_compo/Upload_img';
-import './agePredictor.css';
-import poppassimg2 from '../BabyGenrator_page/babyG-img/poppassimg2.png';
-import star from '../BabyGenrator_page/babyG-img/star.svg'
-import predictorImage from './predictor_image/agepredictor.png';
-import questionMark from '../BabyGenrator_page/babyG-img/question.svg';
-import Howworkpop from '../../components/popUp/how_it_work_pop/Howworkpop';
-import Profileicon1 from '../BabyGenrator_page/babyG-img/profile-1.svg';
-import upload from '../BabyGenrator_page/babyG-img/upload.svg';
-import CropImage from "../../components/CropImage/CropImage";
-import useUploadImg from "../../hooks/useUploadImg";
 import { useState } from "react";
+import usePopup from "../../hooks/usePopup";
+import useUploadImg from "../../hooks/useUploadImg";
+import { agePredictorAPI } from "../../services/imageBase"; 
+
+import Upload_img from "../../components/upload_img_re_compo/Upload_img";
+import CropImage from "../../components/CropImage/CropImage";
+import Howworkpop from "../../components/popUp/how_it_work_pop/Howworkpop";
+
+import "./agePredictor.css";
+
+import poppassimg2 from "../BabyGenrator_page/babyG-img/poppassimg2.png";
+import star from "../BabyGenrator_page/babyG-img/star.svg";
+import predictorImage from "./predictor_image/agepredictor.png";
+import questionMark from "../BabyGenrator_page/babyG-img/question.svg";
+import Profileicon1 from "../BabyGenrator_page/babyG-img/profile-1.svg";
+import upload from "../BabyGenrator_page/babyG-img/upload.svg";
+import { blobUrlToFile } from "../../utils/blobToFile";
 
 
 function AgePredictor() {
-    const { showPopup, handleOpen, handleClose } = usePopup();
-    const parent1Upload = useUploadImg();
+  const { showPopup, handleOpen, handleClose } = usePopup();
+  const parent1Upload = useUploadImg();
 
-    return (
-        <>
-            <div className="main-agePredictor">
-                <div className="left-main-agePredictor">
-                    <div className="inner-left-1-agePredictor">
-                        <h4>AI Age Predictor</h4>
-                        <button onClick={handleOpen} className='btn-pop-up-howWork'>
-                            <img src={questionMark} alt="" />
-                            <span>How It Works</span>
-                        </button>
-                        {showPopup && (
-                            <Howworkpop
-                                howworkpopDetails={{
-                                    onClose: handleClose,
-                                    image: poppassimg2,
-                                    message: "Predict your age with AI. Upload and guess!"
-                                }}
-                            />
-                        )}
-                    </div>
-                    <div className="uplod-image-button-Parent-2">
-                        <label className="uplod-button-agePredictor" htmlFor="parent1Input">
-                            {parent1Upload.croppedImage ? (
-                                <img
-                                    src={parent1Upload.croppedImage}
-                                    alt="Parent 1"
-                                    className="preview-img"
-                                />
-                            ) : (
-                                <>
-                                    <div className="profile-icon-container">
-                                        <img src={Profileicon1} alt="Parent 1 Icon" className='Parent-Icon' />
-                                    </div>
-                                    <p>Parent 1</p>
-                                </>
-                            )}
-                        </label>
 
-                        <div className='img-upload-button-container'>
-                            <input
-                                type="file"
-                                accept="image/*"
-                                id="parent1Input"
-                                className="hidden"
-                                onChange={parent1Upload.handleFileUpload}
-                                disabled={!!parent1Upload.croppedImage}
-                            />
+  const handleGenerate = async () => {
+    if (!parent1Upload.croppedImage) {
+      alert("Please upload both parent images.");
+      return;
+    }
+  
+    const parent1File = await blobUrlToFile(parent1Upload.croppedImage, "agePredictor.png");
+  
+    const imageFiles = {
+      agePredictorUpload: parent1File,
+    };
+  
+    const otherData = {
+      userId: 5,
+      transactionId: 1,
+      Predict_age: 30
+    };
+  
+    try {
+      const response = await agePredictorAPI(imageFiles, otherData);
+      console.log("Response from API:", response);
+    } catch (error) {
+      console.error("Error predicting age:", error);
+      alert("Failed to generate image. Please try again.");
+    }
+  };
 
-                            {!parent1Upload.croppedImage ? (
-                                <label htmlFor="parent1Input" className="uplod-button">
-                                    <img className="upload-img-icon" src={upload} alt="Upload Icon" />
-                                    <p>Upload</p>
-                                </label>
-                            ) : (
-                                <button
-                                    type="button"
-                                    className="uplod-button"
-                                    onClick={() => {
-                                        parent1Upload.resetImage(); 
-                                        const input = document.getElementById("parent1Input");
-                                        if (input) input.value = ""; 
-                                    }}
-                                >
-                                   <img width="10" height="10" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/delete-sign.png" alt="delete-sign"/>
-                                            cancel 
-                                </button>
-                            )}
-                        </div>
+  return (
+    <div className="main-agePredictor">
+      {/* Left Section */}
+      <div className="left-main-agePredictor">
+        {/* Header */}
+        <div className="inner-left-1-agePredictor">
+          <h4>AI Age Predictor</h4>
+          <button onClick={handleOpen} className="btn-pop-up-howWork">
+            <img src={questionMark} alt="Help icon" />
+            <span>How It Works</span>
+          </button>
 
-                        {parent1Upload.showCropper && (
-                            <div className="overlay">
-                                <div className="popup">
-                                    <button
-                                        className="close-btn"
-                                        onClick={() => parent1Upload.setShowCropper(false)}
-                                    >
-                                        <img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/delete-sign.png" alt="delete-sign"/>
-                                            
-                                    </button>
-                                    <CropImage
-                                        imageSrc={parent1Upload.selectedFile}
-                                        onCropDone={parent1Upload.handleCropComplete}
-                                        onCancel={() => parent1Upload.setShowCropper(false)}
-                                    />
-                                </div>
-                            </div>
-                        )}
+          {showPopup && (
+            <Howworkpop
+              howworkpopDetails={{
+                onClose: handleClose,
+                image: poppassimg2,
+                message: "Predict your age with AI. Upload and guess!",
+              }}
+            />
+          )}
+        </div>
 
-                    </div>
-                    <div className="inner-left-3-agePredictor">
-                        <div className='inner-1-for-left-3'>
-                            <p>Est. time: 30 seconds to 50 seconds</p>
-                        </div>
-                        <div className="inner-2-for-left-3">
-                            <button className='agePredictor-left-3-btn-1'>See Pricing</button>
-                            <button className='agePredictor-left-3-btn-2'>
-                                Generate
-                                <div className="agePredictor-left-3-btn-2-icon">
-                                    <img src={star} alt="" />
-                                    <span>-0.5</span>
-                                </div>
-                            </button>
-                        </div>
-                    </div>
+        {/* Upload Section */}
+        <div className="uplod-image-button-Parent-2">
+          <label className="uplod-button-agePredictor" htmlFor="parent1Input">
+            {parent1Upload.croppedImage ? (
+              <img
+                src={parent1Upload.croppedImage}
+                alt="Parent 1"
+                className="preview-img"
+              />
+            ) : (
+              <>
+                <div className="profile-icon-container">
+                  <img
+                    src={Profileicon1}
+                    alt="Parent 1 Icon"
+                    className="Parent-Icon"
+                  />
                 </div>
-                <div className="right-main-agePredictor">
-                    <Upload_img uploadDetails={{ image: predictorImage }} />
-                </div>
+                <p>Parent 1</p>
+              </>
+            )}
+          </label>
+
+          <div className="img-upload-button-container">
+            <input
+              type="file"
+              accept="image/*"
+              id="parent1Input"
+              className="hidden"
+              onChange={parent1Upload.handleFileUpload}
+              disabled={!!parent1Upload.croppedImage}
+            />
+
+            {!parent1Upload.croppedImage ? (
+              <label htmlFor="parent1Input" className="uplod-button">
+                <img className="upload-img-icon" src={upload} alt="Upload" />
+                <p>Upload</p>
+              </label>
+            ) : (
+              <button
+                type="button"
+                className="uplod-button"
+                onClick={() => {
+                  parent1Upload.resetImage();
+                  const input = document.getElementById("parent1Input");
+                  if (input) input.value = "";
+                }}
+              >
+                <img
+                  width="10"
+                  height="10"
+                  src="https://img.icons8.com/ios-glyphs/30/FFFFFF/delete-sign.png"
+                  alt="delete"
+                />
+                Cancel
+              </button>
+            )}
+          </div>
+
+          {/* Crop Popup */}
+          {parent1Upload.showCropper && (
+            <div className="overlay">
+              <div className="popup">
+                <button
+                  className="close-btn"
+                  onClick={() => parent1Upload.setShowCropper(false)}
+                >
+                  <img
+                    width="20"
+                    height="20"
+                    src="https://img.icons8.com/ios-glyphs/30/FFFFFF/delete-sign.png"
+                    alt="close"
+                  />
+                </button>
+                <CropImage
+                  imageSrc={parent1Upload.selectedFile}
+                  onCropDone={parent1Upload.handleCropComplete}
+                  onCancel={() => parent1Upload.setShowCropper(false)}
+                />
+              </div>
             </div>
-        </>
-    )
+          )}
+        </div>
+
+        {/* Footer */}
+        <div className="inner-left-3-agePredictor">
+          <div className="inner-1-for-left-3">
+            <p>Est. time: 30s to 50s</p>
+          </div>
+          <div className="inner-2-for-left-3">
+            <button className="agePredictor-left-3-btn-1">See Pricing</button>
+            <button className="agePredictor-left-3-btn-2" onClick={handleGenerate}>
+              Generate
+              <div className="agePredictor-left-3-btn-2-icon">
+                <img src={star} alt="star" />
+                <span>-0.5</span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="right-main-agePredictor">
+        <Upload_img uploadDetails={{ image: predictorImage }} />
+      </div>
+    </div>
+  );
 }
+
 export default AgePredictor;
--- src/services/imageBase.jsx
+++ src/services/imageBase.jsx
@@ -8,7 +8,6 @@ const api = axios.create({
 export const babyuploadeAPI = async (imageFiles, otherData) => {
   try {
     const formData = new FormData();
-    // Send actual File objects, not blob URLs
     formData.append("parent1", imageFiles.parent1);
     formData.append("parent2", imageFiles.parent2);
     formData.append("userid", otherData.userid);
@@ -16,6 +15,29 @@ export const babyuploadeAPI = async (imageFiles, otherData) => {
     formData.append("transactionId", otherData.transactionId);
 
     const response = await api.post("/baby-generator", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    throw error;
+  }
+};
+
+
+
+export const agePredictorAPI = async (imageFiles, otherData) => {
+  try {
+    const formData = new FormData();
+    formData.append("agePredictorUpload", imageFiles.agePredictorUpload);
+    formData.append("userId", otherData.userId);
+    formData.append("Predict_age", otherData.Predict_age);
+    formData.append("transactionId", otherData.transactionId);
+
+    const response = await api.post("/age-predictor", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
