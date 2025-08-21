f44706b5e9c786d4206af30608e07d301d2992e6
yash-vamja <vamjayash532@gmail.com>
2025-08-21 18:49:56 +0530

    push by yash

--- src/services/imageBase.jsx
+++ src/services/imageBase.jsx
@@ -27,3 +27,27 @@ export const babyuploadeAPI = async (imageFiles, otherData) => {
     throw error;
   }
 };
+
+export const chnageHaircutAPI = async (imageFiles, otherData) => {
+  try {
+    const formData = new FormData();
+    // Send actual File objects, not blob URLs
+    formData.append("HairuploadPhoto", imageFiles.parent1);
+    formData.append("hairStyle", otherData.hairstyle);
+    formData.append("hairColor", otherData.hairColor);
+    formData.append("userid", otherData.userid);
+    formData.append("gender",otherData.gender);
+    formData.append("transactionId", otherData.transactionId);
+    console.log("imageFiles:", imageFiles.parent1);
+    const response = await api.post("/change-hairstyle", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    throw error;
+  }
+}
