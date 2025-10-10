64340a2cd175931ce1c8083d61e8cb9cee8716a3
Abhi Vagadiya <your-email@example.com>
2025-10-10 16:56:01 +0530

    111

--- src/pages/Profile/ImageHistory.jsx
+++ src/pages/Profile/ImageHistory.jsx
@@ -2,41 +2,75 @@ import React, { Component } from 'react'
 import backArrow from "./Profile-image/backArrow.png";
 import ProfileImage from "./Profile-image/Profile-icon.svg";
 
-export class ImageHistory extends Component {
-  render() {
-    return (
-      <div className='image-history-container'>
-        <div className='image-card'>
-            <img src={backArrow} alt="" />
-        </div>
-        <div className='image-card'>
-            <img src={ProfileImage} alt="" />
-        </div>
-        <div className='image-card'>
-            <img src={backArrow} alt="" />
-        </div>
-        <div className='image-card'>
-            <img src={ProfileImage} alt="" />
-        </div>
-        <div className='image-card'>
-            <img src={backArrow} alt="" />
-        </div>
-        <div className='image-card'>
-            <img src={ProfileImage} alt="" />
-        </div>
-        <div className='image-card'>
-            <img src={backArrow} alt="" />
-        </div>
-        <div className='image-card'>
-            <img src={ProfileImage} alt="" />
-        </div>
-        <div className='image-card'>
-            <img src={backArrow} alt="" />
-        </div>
+function ImageHistory () {
 
-      </div>
+    return (
+        <div className='image-history-container'>
+            <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+            <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+            <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+            <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+            <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+            <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+            <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+            <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+            <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={backArrow} alt="" />
+            </div>
+             <div className='image-card'>
+                <img src={ProfileImage} alt="" />
+            </div>
+
+
+        </div>
     )
-  }
 }
+
 
 export default ImageHistory
